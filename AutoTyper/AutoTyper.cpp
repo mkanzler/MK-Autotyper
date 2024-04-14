44be875832286dffa35a6f4038cc6dae02fa2ab0
mkanzler <167009930+mkanzler@users.noreply.github.com>
2024-04-14 19:02:24 +0200

    Added variable delays for keystrokes and initial delay

--- AutoTyper/AutoTyper.cpp
+++ AutoTyper/AutoTyper.cpp
@@ -9,15 +9,15 @@ void PressKey(BYTE keyCode) {
 }
 
 // Function to simulate keystrokes
-void simulateKeystrokes(const std::wstring& input) {
-    Sleep(1000);
+void simulateKeystrokes(const std::wstring& input,const int&keyStrokeDelay, const int& initialDelay) {
+    Sleep(initialDelay);
     HWND foregroundWindow = GetForegroundWindow();
  
     for (char c : input) {
         if (foregroundWindow != GetForegroundWindow()) {
             break;
         }
-        Sleep(10); // Adjust this delay as needed
+        Sleep(keyStrokeDelay); // Can be adjusted
         switch (c) {
         case 'ü':
             PressKey(VK_OEM_1); // 'ü'
@@ -63,29 +63,46 @@ void simulateKeystrokes(const std::wstring& input) {
 }
 
 // Windows procedure function
+HWND textboxTypeContent, txtKeyStrokeDelay, txtInitialDelay;
+std::wstring getTxtBoxString(HWND txtBox) {
+    int length = GetWindowTextLength(txtBox);
+    std::wstring buffer(length + 1, L'\0');
+    GetWindowText(txtBox, &buffer[0], length + 1);
+    std::wstring input(buffer.begin(), buffer.end());
+    return input;
+}
+
+
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
     case WM_CREATE: {
-        // Create textbox
-        HWND textbox = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"", WS_CHILD | WS_VISIBLE | ES_MULTILINE | ES_AUTOVSCROLL | ES_AUTOHSCROLL, 10, 10, 500, 300, hwnd, NULL, GetModuleHandle(NULL), NULL);
-        // Create button
-        CreateWindow(L"BUTTON", L"TYPE", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 10, 320, 100, 30, hwnd, (HMENU)1, GetModuleHandle(NULL), NULL);
+        textboxTypeContent = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"", WS_CHILD | WS_VISIBLE | ES_MULTILINE | ES_AUTOVSCROLL | ES_AUTOHSCROLL, 10, 10, 760, 500, hwnd, NULL, GetModuleHandle(NULL), NULL);
+        CreateWindow(L"BUTTON", L"TYPE", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 10, 520, 100, 30, hwnd, (HMENU)1, GetModuleHandle(NULL), NULL);
+        HWND labelKeyStrokeDelay = CreateWindowEx(0, L"STATIC", L"Keystroke delay (ms):", WS_CHILD | WS_VISIBLE,130, 520, 120, 30,hwnd,NULL,GetModuleHandle(NULL),NULL);
+        txtKeyStrokeDelay = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"10", WS_CHILD | WS_VISIBLE | ES_AUTOVSCROLL | ES_AUTOHSCROLL, 255, 520, 100, 30, hwnd, NULL, GetModuleHandle(NULL), NULL);
+        HWND labelInitialDelay = CreateWindowEx(0, L"STATIC", L"Initial delay (ms):", WS_CHILD | WS_VISIBLE, 385, 520, 120, 30, hwnd, NULL, GetModuleHandle(NULL), NULL);
+        txtInitialDelay = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"1000", WS_CHILD | WS_VISIBLE | ES_AUTOVSCROLL | ES_AUTOHSCROLL, 510, 520, 100, 30, hwnd, NULL, GetModuleHandle(NULL), NULL);
         break;
     }
     case WM_COMMAND: {
         switch (LOWORD(wParam)) {
         case 1: { // ID of the button
-            // Get the text from the textbox
-            HWND textbox = GetDlgItem(hwnd, 0);
-            int length = GetWindowTextLength(textbox);
-            std::wstring buffer(length + 1, L'\0');
-            GetWindowText(textbox, &buffer[0], length + 1);
+            int iKeyStrokeDelay = 10;
+            int iInitialDelay = 1000;
+            try {
+                iKeyStrokeDelay = std::stoi(getTxtBoxString(txtKeyStrokeDelay));
+            }
+            catch (const std::exception& e) {
+                SetWindowText(txtKeyStrokeDelay, L"10");
+            }
+            try {
+                iInitialDelay = std::stoi(getTxtBoxString(txtInitialDelay));
+            }
+            catch (const std::exception& e) {
+                SetWindowText(txtInitialDelay, L"1000");
+            }
 
-            // Convert wide string to narrow string
-            std::wstring input(buffer.begin(), buffer.end());
-
-            // Simulate keystrokes
-            simulateKeystrokes(input);
+            simulateKeystrokes(getTxtBoxString(textboxTypeContent), iKeyStrokeDelay, iInitialDelay);
             break;
         }
         }
@@ -114,7 +131,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     RegisterClass(&wc);
 
     // Create the window
-    HWND hwnd = CreateWindowEx(0, CLASS_NAME, L"MK AutoTyper", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 600, 400, NULL, NULL, hInstance, NULL);
+    HWND hwnd = CreateWindowEx(0, CLASS_NAME, L"MK AutoTyper", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, NULL, NULL, hInstance, NULL);
 
     if (hwnd == NULL) {
         return 0;
